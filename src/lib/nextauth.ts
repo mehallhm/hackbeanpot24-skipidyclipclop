41b9bfc91b7e7068f6e68afa8639a6089ebeb1ce
Mike968 <michaelhmehall@gmail.com>
2024-02-24 12:25:38 -0500

    feat: Authentication flow

---
+++ src/lib/nextauth.ts
@@ -0,0 +1,28 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import type { Adapter } from "next-auth/adapters";
+export const authOptions: NextAuthOptions = {
+  // Secret for Next-auth, without this JWT encryption/decryption won't work
+  secret: process.env.NEXTAUTH_SECRET,
+
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+          scope:
+            "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events.freebusy",
+        },
+      },
+    }),
+  ],
+
+  adapter: MongoDBAdapter(clientPromise) as Adapter,
+};
